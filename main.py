91ac4a2c685739b62e155be409653bd046bbc79e
RydiaT <rydia.a.titus@gmail.com>
2025-07-19 17:32:37 -0400

    Added Scoring

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pygame
 from constants import *
 from player import Player
@@ -6,14 +8,42 @@ from asteroidfield import AsteroidField
 from shot import Shot
 from sys import exit
 
+def get_highscore():
+	if not os.path.exists("highscore.txt"):
+		print("No highscore file found.")
+		f = open("highscore.txt", "x")
+		f = open("highscore.txt", "w")
+		f.write("0")
+		f.close()
+		print("Highscore save created.")
+		print("\n=====================================\n")
+
+	with open("highscore.txt", "r") as score_raw:
+		score = score_raw.read().strip()
+
+		try:
+			return int(score)
+		except ValueError:
+			print("Highscore unable to be read.")
+			print(score)
+			print(type(score))
+			return -1
+
+def set_highscore(score):
+	with open("highscore.txt", "w") as score_raw:
+		score_raw.write(f"{score}")
+
+
 
 def main():
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	print("\nStarting Asteroids!")
+
+	print("\n=====================================\n")
 
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	font = pygame.font.SysFont('Arial', 36)  # Using a system font, size 36
 
 	clock = pygame.time.Clock()
 	dt = 0
@@ -31,6 +61,9 @@ def main():
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	field = AsteroidField()
 
+	score = 0
+	highscore = get_highscore()
+
 	while True:
 		# Lets us actually use the [X] button
 		for event in pygame.event.get():
@@ -41,6 +74,12 @@ def main():
 
 		for thing in asteroids:
 			if thing.is_colliding(player):
+				if score > highscore:
+					print("NEW HIGHSCORE!!!!")
+					print(f"{highscore} ---> {score}")
+
+					set_highscore(score)
+
 				exit("Game over!")
 
 		for thing in asteroids:
@@ -49,8 +88,21 @@ def main():
 					thing.split()
 					bullet.kill()
 
+					score += 1
+
 		# Render
 		screen.fill("black")
+
+		if score > highscore:
+			text_color = "yellow"
+		else:
+			text_color = "white"
+
+		text_surface = font.render(f"{score}", True, text_color)
+		text_rect = text_surface.get_rect()
+		text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)  # Center it on the screen
+
+		screen.blit(text_surface, text_rect)
 
 		for thing in drawable:
 			thing.draw(screen)
