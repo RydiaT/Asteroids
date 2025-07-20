3a06d8fcfd31083000db201ee832a4490f0e8133
RydiaT <rydia.a.titus@gmail.com>
2025-07-19 21:05:33 -0400

    Added Seperate helpers.py File

---
+++ helpers.py
@@ -0,0 +1,42 @@
+from colorama import Fore, Style
+
+def cprint(message, message_type="info"):
+	prefix = ""
+	if message_type == "info":
+		prefix = Fore.CYAN
+	elif message_type == "warning":
+		prefix = Fore.YELLOW
+	elif message_type == "error":
+		prefix = Fore.RED
+	elif message_type == "success":
+		prefix = Fore.GREEN
+
+	print(prefix + message + Style.RESET_ALL)
+
+
+def get_highscore():
+	if not os.path.exists("highscore.txt"):
+		cprint("No highscore file found.", "warning")
+
+		f = open("highscore.txt", "x")
+		f = open("highscore.txt", "w")
+		f.write("0")
+		f.close()
+
+		cprint("Highscore save created.", "info")
+		print("\n=====================================\n")
+
+	with open("highscore.txt", "r") as score_raw:
+		score = score_raw.read().strip()
+
+		try:
+			return int(score)
+		except ValueError:
+			cprint("Highscore unable to be read.", "error")
+			print(score)
+			print(type(score))
+			return -1
+
+def set_highscore(score):
+	with open("highscore.txt", "w") as score_raw:
+		score_raw.write(f"{score}")
